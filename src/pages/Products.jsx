94a38a3b1abb14eeb5f45031318c5087a5bb5701
umitakbas82 <umit.akbas@hotmail.com.tr>
2025-07-31 09:43:20 +0300

    Products Page added

---
+++ src/pages/Products.jsx
@@ -0,0 +1,13 @@
+import DashboardLayout from "../layouts/DashboardLayout";
+
+export default function Product(){
+
+
+    return(
+        <DashboardLayout>
+
+
+            
+        </DashboardLayout>
+    )
+}
