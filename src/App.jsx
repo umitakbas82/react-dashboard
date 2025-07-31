3760cfb020c65391975563834a55a703863c9635
umitakbas82 <umit.akbas@hotmail.com.tr>
2025-07-31 17:05:09 +0300

    çok saçma oldu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from './components/Dashboard'
+import Dashboard from './components/Dashboard/Dashboard'
 import UsersPage from './pages/UsersPage'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Product from './pages/Products';
+import DashboardLayout from './layouts/DashboardLayout';
 
 function App() {
  
@@ -14,9 +15,11 @@ function App() {
     <BrowserRouter>
     
     <Routes>
+    <Route element={<DashboardLayout />}>
       <Route path="/" element={<Dashboard />} />
       <Route path="/users" element={<UsersPage/>}/>
       <Route path="/products" element={<Product/>}/>
+      </Route>
     </Routes>
   </BrowserRouter>
   )
--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,5 +1,4 @@
-// import Sidebar from "../components/Sidebar";
-// import Header from "../components/Header";
+
 
 // export default function DashboardLayout({ children }) {
 //     return (
@@ -15,19 +14,43 @@
 //     );
 //   }
 
-import Sidebar from "../components/Sidebar";
-import Header from "../components/Header";
+// import Sidebar from "../components/Dashboard/Sidebar";
+// import Header from "../components/Dashboard/Header";
 
-export default function DashboardLayout({ children }) {
+// export default function DashboardLayout({ children }) {
 
   
+//   return (
+//     <div className="d-flex vh-100 vw-100 overflow-hidden">
+//       <Sidebar />
+//       <div className="flex-grow-1 d-flex flex-column" >
+//         <Header />
+//         <main className="flex-grow-1 overflow-auto p-4 bg-light">{children}</main>
+//       </div>
+//     </div>
+//   );
+// }
+
+import { useState } from "react";
+import Sidebar from "../components/Dashboard/Sidebar";
+import Header from "../components/Dashboard/Header";
+import { Outlet } from "react-router-dom";
+export default function DashboardLayout({ children }) {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => setSidebarOpen(!isSidebarOpen);
+
   return (
-    <div className="d-flex vh-100 vw-100 overflow-hidden">
-      <Sidebar />
-      <div className="flex-grow-1 d-flex flex-column" >
-        <Header />
-        <main className="flex-grow-1 overflow-auto p-4 bg-light">{children}</main>
+    <div className="d-flex flex-column flex-md-row" style={{ height: "100vh" }}>
+      <Sidebar isOpen={isSidebarOpen} onClose={toggleSidebar} />
+      <div className="flex-grow-1 d-flex flex-column overflow-hidden">
+        <Header onMenuClick={toggleSidebar} />
+        <main className="flex-grow-1 overflow-auto p-3 bg-light">
+          {children}
+          <Outlet />
+        </main>
       </div>
     </div>
   );
 }
+
