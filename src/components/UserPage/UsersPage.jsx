b10ef7b6f296e678af9ebb9a2f892c19a0752457
UmitAkbas <umit.akbas@hotmail.com.tr>
2025-08-05 00:19:53 +0300

    min commit

--- src/components/UserPage/UsersPage.jsx
+++ src/components/UserPage/UsersPage.jsx
@@ -433,7 +433,7 @@ export default function UsersPage() {
         
         <button 
           onClick={() => setIsOffcanvasOpen(true)}
-          className="flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white px-3 py-2 rounded-md font-medium text-sm transition-colors me-3 rounded-xl"
+          className="flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white px-3 py-2 rounded-md font-medium text-sm transition-colors me-3 rounded-lg"
         >
           <svg width="14" height="14" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M9.25 9.25V4.75H10.75V9.25H15.25V10.75H10.75V15.25H9.25V10.75H4.75V9.25H9.25Z" fill="white"/>
