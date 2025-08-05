d92354cf92ed28a281b0f2e567e1e9eaeae39777
umitakbas82 <umit.akbas@hotmail.com.tr>
2025-08-05 11:48:48 +0300

    pages edit

--- src/components/Pages/Pages.jsx
+++ src/components/Pages/Pages.jsx
@@ -216,19 +216,22 @@ export default function UsersPage() {
 
   return (
     <Layout>
-      <div className="content-header mb-4">
-        <div className="header-info">
-          <div className="page-title">
-            <h1>Kullanıcılar</h1>
-            <p>Sistem kullanıcılarını yönetin, yeni kullanıcılar ekleyin ve mevcut kullanıcı bilgilerini güncelleyin.</p>
-          </div>
+        <div className="fixed top-[90px] left-[265px] right-0 z-[5] bg-white px-6 py-2 border-b border-gray-200 flex justify-between items-center">
+        <div className="flex-1">
+          <h1 className="text-lg font-semibold text-gray-900 ms-3">Sayfalar</h1>
+          <p className="text-sm text-gray-600 ms-3">Bu bölümde web sitenizdeki tüm sayfaları oluşturabilir, düzenleyebilir ve yönetebilirsiniz.</p>
         </div>
-        <button className="new-content-button" onClick={() => setIsOffcanvasOpen(true)}>
-          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        
+        <button 
+          onClick={() => setIsOffcanvasOpen(true)}
+          className="flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white px-3 py-2 rounded-md font-medium text-sm transition-colors me-3 rounded-lg"
+        >
+          <svg width="14" height="14" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M9.25 9.25V4.75H10.75V9.25H15.25V10.75H10.75V15.25H9.25V10.75H4.75V9.25H9.25Z" fill="white"/>
           </svg>
-          <span>Yeni Kullanıcı Ekle</span>
+          Yeni Kullanıcı Ekle
         </button>
+ 
       </div>
       <hr className="my-4" />
       <div className="users-container">
